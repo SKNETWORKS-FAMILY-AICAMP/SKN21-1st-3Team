c0c6bb1c2f0f694ea22441a1579c9e7ef04cd036
jse <ya7016@naver.com>
2025-10-23 09:17:29 +0900

    FEAT : setting .env variables

--- src/database.py
+++ src/database.py
@@ -1,15 +1,29 @@
 import pymysql as sql
+from dotenv import load_dotenv
+import os
+
+
+# .env 환경 변수 load
+load_dotenv()
+
+# 환경 변수 설정
+db_host = os.environ.get('DB_HOST')
+db_user = os.environ.get('DB_USER')
+db_password = os.environ.get('DB_PASSWORD')
+db_name = os.environ.get('DB_NAME')
+db_port = int(os.environ.get('DB_PORT'))
+
 
 """
     sql 서버 연동 모듈
 """
 
 connection = sql.connect(
-    host="192.168.0.37",
-    user="project1",
-    password="1111",
-    port=3306,
-    db="elecar_parking",
+    host=db_host,
+    user=db_user,
+    password=db_password,
+    port=db_port,
+    db=db_name,
 )
 
 cursor = connection.cursor()
--- src/get-data.py
+++ src/get-data.py
@@ -1,10 +1,15 @@
 import requests
+from dotenv import load_dotenv
+import os
+
+# .env 환경 변수 load
+
+load_dotenv()
 
 base_url = "https://www.safemap.go.kr/openApiService/data"
-api_key = "0KEH6JLX-0KEH-0KEH-0KEH-0KEH6JLX28"
+api_key = os.environ.get('API_KEY')
 data_size = 1000
 page = 1
-
 
 response = requests.api.get(
     f"{base_url}/getChargingStationData1.do?serviceKey={api_key}&numOfRows={data_size}&pageNo={page}&dataType=JSON",
