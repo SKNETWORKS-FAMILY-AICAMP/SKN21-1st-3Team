0b890d7438106915dacf819bdacd9dcc26b65a0b
TERA <p.tyche92@gmail.com>
2025-10-23 09:23:44 +0900

    [fix] env 타입에러 수정

--- src/database.py
+++ src/database.py
@@ -7,11 +7,11 @@ import os
 load_dotenv()
 
 # 환경 변수 설정
-db_host = os.environ.get('DB_HOST')
-db_user = os.environ.get('DB_USER')
-db_password = os.environ.get('DB_PASSWORD')
-db_name = os.environ.get('DB_NAME')
-db_port = int(os.environ.get('DB_PORT'))
+db_host = os.environ.get("DB_HOST")
+db_user = os.environ.get("DB_USER")
+db_password = os.environ.get("DB_PASSWORD") or ""
+db_name = os.environ.get("DB_NAME")
+db_port = int(os.environ.get("DB_PORT") or 0)
 
 
 """
