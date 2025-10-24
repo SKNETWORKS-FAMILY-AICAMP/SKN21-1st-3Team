01789bed36969754dbe1e204060103c201717ac3
TERA <p.tyche92@gmail.com>
2025-10-24 15:32:24 +0900

    [fix] 지도, 테이블 데이터 매핑

--- src/view/view.py
+++ src/view/view.py
@@ -1,85 +1,95 @@
 import streamlit as st
-from streamlit_folium import st_folium
-import folium as fol
 from region_name import regions
+from streamlit_folium import folium_static
+from folium import folium, plugins, Marker
+from find_region_code import find_region_code, find_sigungu_code
+from get_station_list import get_station_table_list, get_station_map_list
+import pandas as pd
 
 # import find_region_code
 #
 
 st.set_page_config(layout="wide")
 
+if "station_list" not in st.session_state:
+    st.session_state.station_list = []
+
 ########## 페이지 제목 ##########
-st.title("전기차 충전소 찾기")
 
-########## 검색 조건 ##########
-st.subheader("검색 조건")
-st.markdown("지역")
 
-# 첫 번째 선택박스: 도/광역시 선택
-col1, col2 = st.columns(2)
-with col1:
+with st.sidebar:
+    st.title("전기차 충전소 찾기")
+    # 첫 번째 선택박스: 도/광역시 선택
     dosi = st.selectbox(
         "도/시를 선택하세요",
         regions,
     )
-with col2:
     # 조건에 따라 두 번째 selectbox 동적으로 생성
     sigungu = st.selectbox("시/군/구 또는 읍/면/동을 선택하세요", regions[dosi])
 
-st.divider()
-st.markdown(
-    "<style>.area_1 { padding: 8px; border: 1px solid white}</style> <div class='area_1'>",
-    unsafe_allow_html=True,
-)
-col3, col4 = st.columns(2)
-st.markdown("</div>", unsafe_allow_html=True)
-# 24시간 선택 구역
-is_24hr = None
+    st.divider()
+    st.markdown(
+        "<style>.st-key-search_button { margin-top: 20px; }</style>",
+        unsafe_allow_html=True,
+    )
+    # 24시간 선택 구역
 
-with col3:
     st.markdown("운영 시간")
     options = ["지정 시간제 운영", "24시간 운영"]
-    selection = st.pills(
+    selection_time = st.pills(
         "선택 취소 시 버튼 한 번 더 클릭",
         options,
         selection_mode="single",
     )
-    is_24hr = options.index(selection) if selection else None
+    is_24hr = options.index(selection_time) if selection_time else None
 
-
-# 공영주차장 선택 구역
-is_public = None
-with col4:
+    # 공영주차장 선택 구역
+    is_public = None
     st.markdown("주차장 형태")
     options = ["민영주차장", "공영주차장"]
-    selection = st.pills(
+    selection_park = st.pills(
         "선택 취소 시 버튼 한 번 더 클릭",
         options,
         selection_mode="single",
     )
-    is_public = options.index(selection) if selection else None
-
-
-st.markdown("\n")
-st.markdown("\n")
-
-print(is_24hr, is_public)
-st.button("검색", on_click=None, disabled=False, use_container_width=True)
+    is_public = options.index(selection_park) if selection_park else None
 
 
 ########## 지도 데이터 구역 ##########
-st.subheader("검색 결과")
-st.markdown("지도 보기")
-m = fol.Map(location=[37.58403, 126.96997], zoom_start=20)
-## 여러개의 지도 마커
-locations = [
-    (37.58403, 126.96997, "신교공영주차장"),
-    (37.58186, 126.97328, "청와대 사랑채 주차장"),
-    (37.57456, 126.97405, "적선동 공영주차장"),
-]
-for lat, lon, name in locations:
-    fol.Marker([lat, lon], popup=name, icon=fol.Icon(icon="📍")).add_to(m)
-st_folium(m)
+region_code = find_region_code(dosi)
+sigungu_code = find_sigungu_code(sigungu)
+positions = (
+    get_station_map_list(region_code or 0, sigungu_code or 0, is_24hr, is_public) or []
+)
 
+map = folium.Map(location=[37.5652, 126.9774], zoom_start=13)
+map_cluster = plugins.MarkerCluster()
+latitudes = []
+longitudes = []
+
+# 마커 추가
+for row in positions:
+    map_cluster.add_child(Marker(location=[row[1], row[0]], popup=row[2]))
+    latitudes.append(row[1])
+    longitudes.append(row[0])
+map.add_child(map_cluster)
+
+# 마커가 여러개인 경우 마커의 중심으로 지도 이동
+if len(latitudes) > 1:
+    map.fit_bounds(
+        [
+            [min(latitudes), min(longitudes)],
+            [max(latitudes), max(longitudes)],
+        ]
+    )
+# 마커가 한개라면 마커가 지도의 중심
+elif len(latitudes) == 1:
+    map.location = [latitudes[0], longitudes[0]]
+folium_static(map)
 
 # 표 데이터 구역
+st.subheader("상세 목록")
+matrix = pd.DataFrame(
+    get_station_table_list(region_code or 0, sigungu_code or 0, is_24hr, is_public),
+)
+st.dataframe(matrix, width=1000)
